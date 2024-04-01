e7425f1c173efdb2c1f79a34353565f611a68dba
malexa21 <aaliroalexandra@gmail.com>
2024-04-01 23:02:00 +0300

    I implemented the wireframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="bg-[#0A142F]">
+    <main className="sm:grid bg-[#0A142F]">
       <div className="relative min-h-screen">
-        <Image
-          src="/page2.svg"
-          alt="Next.js Logo"
-          layout="fill"
-          objectFit="contain"
-          priority
-        />
+        <div className=" m-8 grid min-h-[1000px] bg-red-700 sm:grid-cols-2">
+          <div className=" m-6 gird min-h-[300px] bg-black"></div>
+          <div className=" m-6 gird min-h-[300px] bg-yellow-400"></div>
+          <div className=" m-6 gird min-h-[100px] min-w-[2000px] bg-green-500"></div>
+        </div>
       </div>
       <div className="relative min-h-[120vh]">
         <Image
