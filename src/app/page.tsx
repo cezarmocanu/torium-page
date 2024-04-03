80ad268e737e1fcd5080da7085bbff18ad0e1e27
malexa21 <aaliroalexandra@gmail.com>
2024-04-03 10:41:29 +0300

    I resolved the comments by adding the same padding and the same gap sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="sm:grid bg-[#0A142F]">
-      <div className="relative min-h-screen items-stretch pb-16">
-        <div className=" m-8 flex-col h-full max-h-fit bg-red-600 grid-cols-2 pb-16 px-2">
-          <div className="grid h-3/5 grid-cols-1 sm:grid-cols-2">
-          <div className=" m-6 min-h-[100px] flex-gap space-y-6 bg-black "/>
-          <div className=" m-6 min-h-[100px] flex-gap bg-yellow-300 "/>
+      <div className="relative min-h-screen items-stretch p-8">
+        <div className="  flex-col h-full max-h-fit bg-red-600 grid-cols-2 p-12">
+          <div className="grid h-3/5 grid-cols-1 sm:grid-cols-2 gap-12 pb-12">
+            <div className=" min-h-[100px] flex-gap space-y-6 bg-black"/>
+            <div className=" min-h-[100px] flex-gap bg-yellow-300 "/>
           </div>
-          <div className=" m-6 min-h-[100px] h-2/5 flex-gap bg-green-600 "/>
+          <div className="min-h-[100px] h-2/5 flex-gap bg-green-600 "/>
         </div>
       </div>
       <div className="relative min-h-[120vh]">
