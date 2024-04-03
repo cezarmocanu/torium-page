8e40e0adbe63232f650ed5abb6194b49e10d236f
malexa21 <aaliroalexandra@gmail.com>
2024-04-03 21:45:40 +0300

    I added the text with the proper size and padding, gap and other details. I have to work on alignment as an ongoing task for the title section of the page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,23 @@ export default function Home() {
     <main className="sm:grid bg-[#0A142F]">
       <div className="relative min-h-screen items-stretch p-8">
         <div className="  flex-col h-full max-h-fit bg-red-600 grid-cols-2 p-12">
+
           <div className="grid h-3/5 grid-cols-1 sm:grid-cols-2 gap-12 pb-12">
-            <div className=" min-h-[100px] flex-gap space-y-6 bg-black"/>
+            <div className=" min-h-[100px] flex-gap space-y-6 bg-black p-12 ">
+              <div className="text-6xl sm:text-md font-medium flex gap-6 text-right">
+                Design. Think.
+                </div>
+              <div className="text-6xl  font-medium flex gap-6 text-right">
+                Solve. Develop.
+              </div>
+              <div className="text">
+                Embark on a journey of inovation with us. Transform ideas into digital solutions that redefine the landscape. 
+                Dive into coding with a purpose, crafting experiences and solving problems. Start building the future today.
+              </div>
+            </div>
             <div className=" min-h-[100px] flex-gap bg-yellow-300 "/>
           </div>
+
           <div className="min-h-[100px] h-2/5 flex-gap bg-green-600 "/>
         </div>
       </div>
