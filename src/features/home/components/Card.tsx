1eb7ebc0c3ff349a00dc4bbc10357751beae9831
malexa21 <aaliroalexandra@gmail.com>
2024-04-06 17:01:46 +0300

    Added desiered changes to the cards

--- src/features/home/components/Card.tsx
+++ src/features/home/components/Card.tsx
@@ -8,8 +8,8 @@ interface Props {
 
 export const Card: FC<Props> = ({title, image, content,}) => {
   return(
-    <div className='flex flex-1 items-center gap-6'>
-      <img className='bg-red-400 h-[50px] w-[50px] items-start rounded-md' src={image}/>
+    <div className='flex flex-1 gap-6'>
+      <img className='bg-red-400 h-[50px] w-[50px] rounded-md'/>
       <div className='flex-1'>
         <h1 className='text-xl'>{title}</h1>
         <p>{content}</p>
