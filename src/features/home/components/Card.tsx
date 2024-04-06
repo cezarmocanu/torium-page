6ba7a58601aab2ddc37ba823b0094ee3ed216901
malexa21 <aaliroalexandra@gmail.com>
2024-04-06 16:32:09 +0300

    Rounded the icon for better placement

--- src/features/home/components/Card.tsx
+++ src/features/home/components/Card.tsx
@@ -9,7 +9,7 @@ interface Props {
 export const Card: FC<Props> = ({title, image, content,}) => {
   return(
     <div className='flex flex-1 items-center gap-6'>
-      <img className='bg-red-400 h-[50px] w-[50px] items-start' src={image}/>
+      <img className='bg-red-400 h-[50px] w-[50px] items-start rounded-md' src={image}/>
       <div className='flex-1'>
         <h1 className='text-xl'>{title}</h1>
         <p>{content}</p>
